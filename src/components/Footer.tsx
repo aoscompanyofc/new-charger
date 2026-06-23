99d820faa59a3ec6187fe31f3c5aebc2abc542e6
Arthur Oliveira <arthuroliver0610@gmail.com>
2026-06-23 11:42:29 -0300

    fix: increase footer logo size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -55,7 +55,7 @@ export default function Footer({ onScrollToForm }: FooterProps) {
                   alt="NewCharged"
                   width="180"
                   height="72"
-                  className="h-16 sm:h-20 w-auto object-contain"
+                  className="h-20 sm:h-28 w-auto object-contain"
                   style={{ mixBlendMode: 'screen' }}
                 />
                 <p className="text-white/25 text-[10px] mt-3">NEW Engenharia e Energia</p>
